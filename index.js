0bf13b60c930154a225461e54710a155bba5e0e5
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2022-10-29 22:03:48 +0530

    basic

---
+++ index.js
@@ -0,0 +1,27 @@
+// comments in javascript
+
+/*
+this is multiple line comment 
+
+*/
+
+document.write("This is an external javascript file");
+
+
+document.getElementById('th').innerHTML = 'This is a paragraph';
+
+var a1 = 4;
+var a2 = 'this is abhishek';
+document.write(a1);
+document.write(a2);
+
+
+// simple calcultaor
+        let v1 = 25;
+        let v2 = 10;
+        let add = v1 + v2;
+        let sub = v1 - v2;
+        let mul = v1 * v2;
+        var div = v1 / v2;
+
+       document.write("Addition is", v1, "Subtraction is ", sub, "Multiplication is ", mul, "Division is ", div);
